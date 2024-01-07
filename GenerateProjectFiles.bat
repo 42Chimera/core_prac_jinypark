f22cfbdd2f490f2bc34141ef251323a9b0ca46d5
learnershigh <jingnuoy508@naver.com>
2024-01-07 21:48:08 +0900

    Create example apps

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,12 +1,8 @@
 @echo off
 
 if not exist "%~dp0Engine/CMakeLists.txt" goto Error_BatchFileInWrongLocation
-cmake -S "%~dp0Engine" -B "%~dp0Engine/Intermediate" -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=ON
+cmake -S "%~dp0Engine" -B "%~dp0Engine/Intermediate" -DBUILD_SHARED_LIBS=ON -G "MinGW Makefiles" -DCMAKE_VERBOSE_MAKEFILE:BOOL=TRUE
 cmake --build "%~dp0Engine/Intermediate"
-@REM make -C "%~dp0Engine/Intermediate" clean
-mkdir "%~dp0Engine/Config"
-echo CHIMERA_ROOT=%~dp0Engine> %~dp0Engine/Config/DefaultEngine.ini
-echo CHIMERA_PLUGINS=%~dp0Engine/Plugins>> %~dp0Engine/Config/DefaultEngine.ini
 exit /B %ERRORLEVEL%
 
 :Error_BatchFileInWrongLocation
